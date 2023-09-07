54ab70179d0e8cdd3ba501545909e3dfad6538ea
Syed Anwar <37025748+anwarkhan0@users.noreply.github.com>
2023-09-07 16:56:07 +0500

    Delete backend/build directory

--- backend/build/routes/cars.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const carRoutes = (0, express_1.Router)();
-carRoutes.get('/cars', (req, res) => {
-    res.send('Hello, cars!');
-});
-exports.default = carRoutes;
